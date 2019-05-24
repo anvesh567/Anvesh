218bd83ddade96bdf4a5b6d1990afb9583150174
anvesh567 <50978383+anvesh567@users.noreply.github.com>
2019-05-24 16:56:58 +0530

    Add files via upload

---
+++ Hibernate Annotations Based/SampleHibernate/src/main/java/com/pawana/test/TestHibernate.java
@@ -0,0 +1,23 @@
+package com.pawana.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernate {
+
+	public static void main(String[] args) {
+		final SessionFactory sessionFactory;
+		sessionFactory = new AnnotationConfiguration().configure().addAnnotatedClass(Employee.class)
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		Employee em1 = new Employee("Mary Smith2", 27);
+		session.save(em1);
+		tx.commit();
+		session.close();
+	}
+
+}
